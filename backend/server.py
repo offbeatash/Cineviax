0b62aac537632b7bf0a4ed744933ae630fc9df00
Ashish Pise <aspise1817@gmail.com>
2026-03-04 19:50:37 +0530

    modified:   backend/server.py

--- backend/server.py
+++ backend/server.py
@@ -306,3 +306,7 @@ app.add_middleware(
 @app.on_event("shutdown")
 async def shutdown_db_client():
     client.close()
+
+@app.get("/")
+async def root():
+    return {"message": "Cineviax API running"}
